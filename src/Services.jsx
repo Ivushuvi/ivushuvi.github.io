49125a5298c6a853efa5f0e668e9f5c5b1cebe63
Drason Guo <drason2015@gmail.com>
2024-03-08 11:36:45 +1300

    Parallax scrolling to services section

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,6 +1,7 @@
 import { Apple, Code, Crop } from "@mui/icons-material";
 import styles from "./Services.module.css"
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { Parallax, useParallax } from "react-scroll-parallax";
 
 export default function Services() {
 
@@ -10,19 +11,42 @@ export default function Services() {
         setTabval(newValue);
     }
 
+    const parallax = useParallax({
+        onProgressChange: (progress) => {
+            if(parallax.ref.current){
+                if(progress < 0.7){
+                    parallax.ref.current.style.setProperty("translate",`0 -${400*((0.7-progress)/0.7)}%`);
+                    parallax2.current.style.setProperty("translate",`0 -${300*((0.7-progress)/0.7)}%`);
+                    parallax3.current.style.setProperty("translate",`0 -${200*((0.7-progress)/0.7)}%`);
+                }else{
+                    parallax.ref.current.style.setProperty("translate",`0 0`);
+                }
+            }
+        }
+    });
+
+    const parallax2 = useRef();
+    const parallax3 = useRef();
+
     return(
         <div>
             <div className={styles.content}>
                 <h1>My services</h1>
                 <div className={styles.tabTitles}>
-                    <div style={tabval==0 ? {backgroundColor:"#444444"} : null} onClick={(e)=>handleChange(0)}>
-                        <h2>Skills</h2>
+                    <div ref={parallax.ref} style={{translate:"0 -400%"}}>
+                        <div style={tabval==0 ? {backgroundColor:"#444444"} : null} onClick={(e)=>handleChange(0)}>
+                            <h2>Skills</h2>
+                        </div>
                     </div>
-                    <div style={tabval==1 ? {backgroundColor:"#444444"} : null} onClick={(e)=>handleChange(1)}>
-                        <h2>Experience</h2>
+                    <div ref={parallax2} style={{translate:"0 -300%"}}>
+                        <div style={tabval==1 ? {backgroundColor:"#444444"} : null} onClick={(e)=>handleChange(1)}>
+                            <h2>Experience</h2>
+                        </div>
                     </div>
-                    <div style={tabval==2 ? {backgroundColor:"#444444"} : null} onClick={(e)=>handleChange(2)}>
-                        <h2>Education</h2>
+                    <div ref={parallax3} style={{translate:"0 -200%"}}>
+                        <div style={tabval==2 ? {backgroundColor:"#444444"} : null} onClick={(e)=>handleChange(2)}>
+                            <h2>Education</h2>
+                        </div>
                     </div>
                 </div>
                 <div className={styles.tabContents}>
